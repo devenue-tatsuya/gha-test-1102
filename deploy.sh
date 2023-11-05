b5b65be68851e2ca535f9265ab75deb49f41aaaf
tatsuya <dosa@dosanombp.lan>
2023-11-05 17:10:59 +0900

    refactoring

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-echo "111111"
-
-ls
-
-echo "222222"
-
-ls client/dist
-
-echo "33333"
-
 
 rsync -acvz --delete ./client/dist/client/ centos@ik1-305-12529.vs.sakura.ne.jp:/usr/share/nginx/html
 
-
-
-echo "Hello, World!"
+echo "Success!"
