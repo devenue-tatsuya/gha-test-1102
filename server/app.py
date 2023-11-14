886d93e12fd33b38f5344158a408be4caefb460b
tatsuya <dosa@dosanombp.lan>
2023-11-14 18:54:47 +0900

    server: ヒーローの一覧をdbから取得して返却

--- server/app.py
+++ server/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+import pymysql
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/api/heroes", methods=['GET'])
-def hoge():
-    return {"name": "Yamada"}
+def getHeroes():
+    db = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        db='hero',
+        charset='utf8',
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    cur = db.cursor()
+    sql = "select * from heroes"
+    cur.execute(sql)
+    result = cur.fetchall()
+
+    return result
