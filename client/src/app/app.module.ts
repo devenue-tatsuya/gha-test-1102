448477d93792529b58cc1016d3959652da55748a
tatsuya <dosa@dosanombp.lan>
2023-11-17 08:58:54 +0900

    HeaderComponent作成

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
