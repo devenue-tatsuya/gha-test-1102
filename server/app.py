b81bf384dcc2231f9377393aaaad465d979fda3a
tatsuya <dosa@dosanombp.lan>
2023-11-26 20:51:52 +0900

    hero作成できるように

--- server/app.py
+++ server/app.py
@@ -1,17 +1,48 @@
 from flask import Flask
-from flask import make_response
+from flask import make_response, request
 from flask_cors import CORS
+import pymysql
 
 from db.core import get_database_config
 
 app = Flask(__name__)
 CORS(app)
 
+connection =  pymysql.connect(
+        host='localhost',
+        user='root',
+        password='password',
+        db='hero',
+        charset='utf8',
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+
 @app.route("/api/heroes", methods=['GET'])
 def get_heroes():
-    cursor = get_database_config()
-    sql = "select * from heroes"
-    cursor.execute(sql)
-    result = cursor.fetchall()
+    with getConnection().cursor() as cursor:
+        sql = "select * from heroes"
+        cursor.execute(sql)
+        result = cursor.fetchall()
 
-    return make_response(result)
+        return make_response(result)
+
+@app.route("/api/heroes", methods=['POST'])
+def create_hero():
+    connection =  pymysql.connect(
+        host='localhost',
+        user='root',
+        password='password',
+        db='hero',
+        charset='utf8',
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO heroes (name) VALUES (%s)"
+            cursor.execute(sql, (request.json['name']['userName']))
+            connection.commit()
+
+    return make_response('hoge')
+
